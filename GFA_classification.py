384cb0968993228d0d24240d74919c423796a6f4
scoronepion <scoronepion@gmail.com>
2019-12-18 21:19:01 +0800

    分类器

--- GFA_classification.py
+++ GFA_classification.py
@@ -32,7 +32,6 @@ class scaled_dot_product_attention(nn.Module):
         context = torch.einsum('ijl,ilk->ijk', [attention, v])
         return context, attention
 
-# TODO:调试
 class multi_heads_self_attention(nn.Module):
     def __init__(self, feature_dim=56, num_heads=4, dropout=0.0):
         super(multi_heads_self_attention, self).__init__()
@@ -47,7 +46,9 @@ class multi_heads_self_attention(nn.Module):
         self.linear_attention = nn.Linear(feature_dim, feature_dim)
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(feature_dim)
-        self.layer_softmax = nn.Linear(feature_dim, 3)
+        # self.linear_1 = nn.Linear(feature_dim, 256)
+        # self.linear_2 = nn.Linear(256, feature_dim)
+        # self.layer_final = nn.Linear(feature_dim, 3)
 
     def forward(self, key, value, query):
         residual = query
@@ -74,11 +75,40 @@ class multi_heads_self_attention(nn.Module):
         # add residual and norm layer
         output = self.layer_norm(residual + output)
 
-        # pass through layer softmax
-        output = self.layer_softmax(output)
-        # output = nn.functional.softmax(output, dim=-1)
+        # # pass through linear
+        # output = nn.functional.relu(self.linear_1(output))
+        # output = nn.functional.relu(self.linear_2(output))
+
+        # # pass through layer final
+        # output = self.layer_final(output)
 
         return output, attention
+
+class gfa_classifier(nn.Module):
+    def __init__(self, feature_dim=56):
+        super(gfa_classifier, self).__init__()
+
+        self.layer_attention_1 = multi_heads_self_attention()
+        self.layer_attention_2 = multi_heads_self_attention()
+
+        self.linear_1 = nn.Linear(feature_dim, 256)
+        self.linear_2 = nn.Linear(256, feature_dim)
+
+        self.layer_final = nn.Linear(feature_dim, 3)
+
+    def forward(self, context):
+        # pass through attention
+        output, attention = self.layer_attention_1(context, context, context)
+        output, _ = self.layer_attention_2(output, output, output)
+
+        # pass through linear
+        output = nn.functional.relu(self.linear_1(output))
+        output = nn.functional.relu(self.linear_2(output))
+
+        # pass through layer final
+        output = self.layer_final(output)
+
+        return output
 
 if __name__ == '__main__':
     with open('/home/lab106/zy/MatTime/GFA_trans.pk', 'rb') as f:
@@ -97,19 +127,52 @@ if __name__ == '__main__':
     y_train = torch.from_numpy(y_train).view(batch_size, -1).long().to(device)
     y_test = torch.from_numpy(y_test).view(batch_size, -1).long().to(device)
 
-    multi_att = multi_heads_self_attention()
-    multi_att.to(device)
-    multi_att.double()
-    # criterion = nn.MSELoss()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(multi_att.parameters())
+    # # 多头注意力
+    # multi_att = multi_heads_self_attention()
+    # multi_att.to(device)
+    # multi_att.double()
+    # # criterion = nn.MSELoss()
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.Adam(multi_att.parameters())
 
-    epoch_num = 2000
+    # epoch_num = 400
+
+    # for epoch in range(epoch_num):
+    #     def closure():
+    #         optimizer.zero_grad()
+    #         out, _ = multi_att(x_train, x_train, x_train)
+    #         loss = criterion(torch.squeeze(out), torch.squeeze(y_train))
+    #         # print('loss:', loss.data.item())
+    #         # loss_list.append(loss.data.item())
+    #         loss.backward()
+    #         return loss
+
+    #     optimizer.step(closure)
+
+    #     if epoch % 10 == 9:
+    #         print('epoch : ', epoch)
+    #         pred, _ = multi_att(x_test, x_test, x_test)
+    #         loss = criterion(torch.squeeze(pred), torch.squeeze(y_test))
+    #         print('test loss:', loss.data.item())
+    #         pred = torch.topk(pred.data.cpu(), 1)[1].squeeze()
+    #         target_names = ['BMG', 'CRA', 'RMG']
+    #         # print('r2:', r2_score(torch.squeeze(y_test.cpu()), torch.squeeze(pred.data.cpu())))
+    #         print(classification_report(torch.squeeze(y_test.cpu()), pred, target_names=target_names))
+
+    gfa = gfa_classifier()
+    gfa.to(device)
+    gfa.double()
+    # criterion = nn.MSELoss()
+    # weight = torch.tensor([1, 3.16, 1], dtype=torch.float64).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(gfa.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-10)
+
+    epoch_num = 10000
 
     for epoch in range(epoch_num):
         def closure():
             optimizer.zero_grad()
-            out, _ = multi_att(x_train, x_train, x_train)
+            out = gfa(x_train)
             loss = criterion(torch.squeeze(out), torch.squeeze(y_train))
             # print('loss:', loss.data.item())
             # loss_list.append(loss.data.item())
@@ -118,9 +181,9 @@ if __name__ == '__main__':
 
         optimizer.step(closure)
 
-        if epoch % 100 == 9:
+        if epoch % 100 == 99:
             print('epoch : ', epoch)
-            pred, _ = multi_att(x_test, x_test, x_test)
+            pred = gfa(x_test)
             loss = criterion(torch.squeeze(pred), torch.squeeze(y_test))
             print('test loss:', loss.data.item())
             pred = torch.topk(pred.data.cpu(), 1)[1].squeeze()
