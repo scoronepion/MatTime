f7ef58342fd604d3ca02a760079381c2f6edaabe
yizeng <scoronepion@gmail.com>
2020-01-27 13:52:13 +0800

    sad story, fix attention

--- cross_element2dmax.py
+++ cross_element2dmax.py
@@ -22,6 +22,85 @@ class cross_layer(nn.Module):
 
         return x_trans + self.b + x
 
+class scaled_dot_product_attention(nn.Module):
+    def __init__(self, att_dropout=0.0):
+        super(scaled_dot_product_attention, self).__init__()
+        self.dropout = nn.Dropout(att_dropout)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, q, k, v, scale=None):
+        '''
+        args:
+            q: [batch_size, q_length, q_dimension]
+            k: [batch_size, k_length, k_dimension]
+            v: [batch_size, v_length, v_dimension]
+            q_dimension = k_dimension = v_dimension
+            scale: 缩放因子
+        return:
+            context, attention
+        '''
+        # 快使用神奇的爱因斯坦求和约定吧！
+        attention = torch.einsum('ijk,ilk->ijl', [q, k])
+        if scale:
+            attention = attention * scale
+        attention = self.softmax(attention)
+        attention = self.dropout(attention)
+        context = torch.einsum('ijl,ilk->ijk', [attention, v])
+        return context, attention
+
+class multi_heads_self_attention(nn.Module):
+    def __init__(self, feature_dim=56, num_heads=2, dropout=0.0):
+        super(multi_heads_self_attention, self).__init__()
+
+        self.dim_per_head = feature_dim // num_heads
+        self.num_heads = num_heads
+        self.linear_q = nn.Linear(feature_dim, self.dim_per_head * num_heads)
+        self.linear_k = nn.Linear(feature_dim, self.dim_per_head * num_heads)
+        self.linear_v = nn.Linear(feature_dim, self.dim_per_head * num_heads)
+
+        self.sdp_attention = scaled_dot_product_attention(dropout)
+        self.linear_attention = nn.Linear(feature_dim, feature_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(feature_dim)
+        # self.linear_1 = nn.Linear(feature_dim, 256)
+        # self.linear_2 = nn.Linear(256, feature_dim)
+        # self.layer_final = nn.Linear(feature_dim, 3)
+
+    def forward(self, key, value, query):
+        residual = query
+        batch_size = key.size(0)
+
+        key = self.linear_k(key)
+        value = self.linear_v(value)
+        query = self.linear_q(query)
+
+        # split by heads
+        key = key.view(batch_size * self.num_heads, -1, self.dim_per_head)
+        value = value.view(batch_size * self.num_heads, -1, self.dim_per_head)
+        query = query.view(batch_size * self.num_heads, -1, self.dim_per_head)
+
+        scale = (key.size(-1) // self.num_heads) ** -0.5
+        context, attention = self.sdp_attention(query, key, value, scale)
+
+        # concat heads
+        context = context.view(batch_size, -1, self.dim_per_head * self.num_heads)
+
+        output = self.linear_attention(context)
+        output = self.dropout(output)
+        output = torch.squeeze(output)
+
+        # add residual and norm layer
+        output = self.layer_norm(residual + output)
+
+        # # pass through linear
+        # output = nn.functional.relu(self.linear_1(output))
+        # output = nn.functional.relu(self.linear_2(output))
+
+        # # pass through layer final
+        # output = self.layer_final(output)
+
+        return output, attention
+
 
 class mlp(nn.Module):
     def __init__(self, input_dim, output_dim):
@@ -30,7 +109,7 @@ class mlp(nn.Module):
         self.linear2 = nn.Linear(256, 512)
         self.linear3 = nn.Linear(512, 256)
         self.linear4 = nn.Linear(256, output_dim)
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, input):
         output = nn.functional.relu(self.linear1(input))
@@ -55,7 +134,7 @@ class cross_mlp(nn.Module):
         self.linear1 = nn.Linear(input_dim + output_dim, 128)
         self.linear2 = nn.Linear(128, 256)
         self.linear3 = nn.Linear(256, 1)
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, input):
         cross_out = self.cross1(input, input)
@@ -70,6 +149,27 @@ class cross_mlp(nn.Module):
         output = nn.functional.relu(self.linear2(output))
         output = self.dropout(output)
         output = self.linear3(output)
+
+        return output
+
+class cross_attention(nn.Module):
+    def __init__(self, input_dim):
+        super(cross_attention, self).__init__()
+        self.cross1 = cross_layer(input_dim)
+        self.cross2 = cross_layer(input_dim)
+        self.cross3 = cross_layer(input_dim)
+        self.cross4 = cross_layer(input_dim)
+        self.attention_layer = multi_heads_self_attention(feature_dim=input_dim, num_heads=2)
+        self.final_linear = nn.Linear(input_dim * 2, 1)
+
+    def forward(self, input):
+        cross_out = self.cross1(input, input)
+        cross_out = self.cross2(input, cross_out)
+        cross_out = self.cross3(input, cross_out)
+        cross_out = self.cross4(input, cross_out)
+        attention_out, _ = self.attention_layer(input, input, input)
+        cat_res = torch.cat((cross_out, attention_out), dim=-1)
+        output = self.final_linear(cat_res)
 
         return output
 
@@ -89,7 +189,7 @@ if __name__ == '__main__':
     y_train = torch.from_numpy(y_train)
     y_test = torch.from_numpy(y_test)
 
-    model = cross_mlp(input_dim=56, output_dim=56)
+    model = cross_attention(input_dim=56)
     model.double()
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
