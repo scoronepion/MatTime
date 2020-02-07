1af8977a82b42aa013573296d756fa18a068d2f6
yizeng <scoronepion@gmail.com>
2020-02-07 23:03:46 +0800

    change dataset to ctt

--- big_predata.py
+++ big_predata.py
@@ -21,14 +21,23 @@ def get_element_set():
 def read_element(noise=False, sort=False, rare_element_scaler=None, nega_sampling=False):
     '''直接返回元素含量百分比'''
     print("start reading...")
-    raw = pd.read_csv('dmax.csv')
-    # quality_table = pd.read_csv('/home/lab106/zy/MatTime/elements.csv')
-    features = pd.DataFrame(columns=['Ba', 'Mn', 'Dy', 'Fe', 'Ca', 'V', 'Al', 'Co', 'Sr', 'Pb', 'T', 'Rh',
-                                    'In', 'Tm', 'Cr', 'W', 'U', 'Ho', 'Lu', 'Ce', 'Nb', 'Mo', 'C', 'Ni', 
-                                    'Li', 'Sm', 'Be', 'Pt', 'Ta', 'Er', 'La', 'Y', 'Sb', 'Yb', 'Zr', 'Sc', 
-                                    'Ir', 'Ag', 'Pr', 'P', 'B', 'Zn', 'Ga', 'Ge', 'Au', 'Sn', 'Cu', 'Nd', 
-                                    'Tb', 'Si', 'Pd', 'Ru', 'Ti', 'Hf', 'Mg', 'Gd'])
+    raw = pd.read_csv('ctt_clean.csv')
+
+    # # elements for dmax.csv
+    # features = pd.DataFrame(columns=['Ba', 'Mn', 'Dy', 'Fe', 'Ca', 'V', 'Al', 'Co', 'Sr', 'Pb', 'T', 'Rh',
+    #                                 'In', 'Tm', 'Cr', 'W', 'U', 'Ho', 'Lu', 'Ce', 'Nb', 'Mo', 'C', 'Ni', 
+    #                                 'Li', 'Sm', 'Be', 'Pt', 'Ta', 'Er', 'La', 'Y', 'Sb', 'Yb', 'Zr', 'Sc', 
+    #                                 'Ir', 'Ag', 'Pr', 'P', 'B', 'Zn', 'Ga', 'Ge', 'Au', 'Sn', 'Cu', 'Nd', 
+    #                                 'Tb', 'Si', 'Pd', 'Ru', 'Ti', 'Hf', 'Mg', 'Gd'])
+
+    # elements for ctt_clean.csv
+    features = pd.DataFrame(columns=['Zn', 'Ho', 'Cu', 'Be', 'Pd', 'Cr', 'Lu', 'Fe', 'Si', 'Li', 'Sn', 
+                                     'Ta', 'Pr', 'In', 'Er', 'C', 'La', 'Pb', 'Ga', 'B', 'Nb', 'Ni', 
+                                     'Al', 'Zr', 'Gd', 'Au', 'Ce', 'Tb', 'Dy', 'W', 'Yb', 'Mo', 'Tm', 
+                                     'Sc', 'Y', 'Mn', 'P', 'Hf', 'Sm', 'Mg', 'Ti', 'Ag', 'Ca', 'Nd', 'Co'])
+
     # # 为稀有元素添加权重
+    # quality_table = pd.read_csv('/home/lab106/zy/MatTime/elements.csv')
     # if rare_element_scaler:
     #     rare_element = ['Sc', 'Y', 'La', 'Ce', 'Pr', 'Nd', 'Sm', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu']
     #     quality_table.loc[quality_table['element'].isin(rare_element), ['quality']] *= rare_element_scaler
@@ -79,25 +88,15 @@ def read_element(noise=False, sort=False, rare_element_scaler=None, nega_samplin
         # 将 dmax 为 0.1 的 3708 条样本负采样为原来的 0.185
         features.drop(features[features['Dmax'] == 0.1].sample(frac=0.815, axis=0).index, inplace=True)
 
-    # 将 dmax 扩大 10 倍
-    features['Dmax'] *= 10
+    # # 将 dmax 扩大 10 倍
+    # features['Dmax'] *= 10
 
-    # print(features.tail(5))
-    # print(features.info())
+    print(features.tail(5))
+    print(features.info())
 
     print('finish read')
     return features.dropna()
 
 if __name__ == '__main__':
-    raw = read_element(noise=False, sort=False, nega_sampling=True)
-    info = dict(raw['Dmax'].value_counts())
-    sum = 0.0
-    for key, value in info.items():
-        if key == 0:
-            print(key, value)
-        elif key == 1:
-            print(key, value)
-        else:
-            sum += value
-    print(sum)
+    raw = read_element(sort=True)
     # get_element_set()
